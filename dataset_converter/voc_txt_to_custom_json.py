f94cb9810866fe1eea99c22beed0f94af6284870
Rudy <rudy.dev.kor@gmail.com>
2021-09-29 17:32:29 +0900

    Update voc_txt_to_custom_json
     - Add add_parent_dir_name param

--- dataset_converter/voc_txt_to_custom_json.py
+++ dataset_converter/voc_txt_to_custom_json.py
@@ -5,18 +5,22 @@ from collections import defaultdict
 import cv2
 
 
-def convert_voc_txt_to_custom_json(input_txt, output_json):
+def convert_voc_txt_to_custom_json(input_txt, image_dir, output_json,
+                                   label_file='',
+                                   add_parent_dir_name=True):
     label_dict = defaultdict(lambda: {})
+
+    parent_dir_name = os.path.basename(image_dir)
 
     with open(input_txt, 'r') as f_in:
         for line in f_in.readlines():
             split = line.split()
-            file_path = split[0]
-            img = cv2.imread(file_path)
-            if img is None:
+
+            image = cv2.imread(os.path.join(image_dir, split[0]))
+            if image is None:
                 continue
 
-            h, w = img.shape[:2]
+            h, w = image.shape[:2]
             class_ids = []
             box_list = []
             for rect in split[1:]:
@@ -35,7 +39,12 @@ def convert_voc_txt_to_custom_json(input_txt, output_json):
                 'box_list': box_list
             }
 
-            label_dict[file_path]['detection_label'] = label
+            if add_parent_dir_name:
+                image_name = os.path.join(parent_dir_name, split[0])
+            else:
+                image_name = split[0]
+
+            label_dict[image_name]['detection_label'] = label
 
     with open(output_json, 'w', encoding='utf-8') as f_out:
         json.dump(label_dict, f_out, ensure_ascii=False, indent=4)
@@ -43,6 +52,8 @@ def convert_voc_txt_to_custom_json(input_txt, output_json):
 
 if __name__ == '__main__':
     convert_voc_txt_to_custom_json(
-        input_txt='/Users/rudy/Desktop/plate_dataset/annotation.txt', 
-        output_json='/Users/rudy/Desktop/plate_dataset/annotation.json'
+        input_txt='/Users/rudy/Desktop/Development/virtualenv/dataset-converter/total(dk+sh+google)_recognizable_plate.txt',
+        image_dir='/Users/rudy/Desktop/Development/dataset/license_plate/dataset/labeled/total(dk+sh+google)', 
+        output_json='/Users/rudy/Desktop/Development/virtualenv/dataset-converter/total(dk+sh+google)_recognizable_plate.json',
+        add_parent_dir_name=True
     )
